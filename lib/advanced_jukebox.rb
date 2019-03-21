abce2d33d93958019ca24bfbd924bcb6983b0a21
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-21 21:42:43 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -55,7 +55,7 @@ def play(my_songs)
   if list_of_songs.include?(response)
     my_songs.each do |song, path|
       if song == response
-        system 'open #{path.slice!(" path to this directory >")}'
+        system 'open #{path.slice!(" path to this directory ")}'
       end
     end
   else
